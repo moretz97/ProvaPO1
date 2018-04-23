38d6fbe8446c6f2baed00bc7bf3b5f4c674f7be2
moretz97 <moretto.riccardo9701@gmail.com>
2018-04-23 18:30:13 +0200

    implementazione classi
    intefaccia graph con errore

--- src/prova/po/Graph.java
+++ src/prova/po/Graph.java
@@ -1,5 +1,19 @@
 package prova.po;
 
-public interface Graph {
+public interface Graph<V,E> {
+
+//operazioni per nodi
+
+    boolean addNode(V);
+
+    boolean deleteNode(V);
+
+    GNode<V> searchNode(V);
+
+//operazioni per archi
+
+    boolean addEdge(GEdge<E,V>);
+
+    boolean removeEdge(GEdge<E,V>);
 
 }

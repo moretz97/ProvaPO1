5cbb46ca970f5ae0d26b19ec452f41bfbfe8e2c8
moretz97 <moretto.riccardo9701@gmail.com>
2018-05-02 10:07:10 +0200

    Update interfaccia

--- src/prova/po/Graph.java
+++ src/prova/po/Graph.java
@@ -4,16 +4,26 @@ public interface Graph<V,E> {
 
 //operazioni per nodi
 
-    boolean aggiungiNode(V n);
+    boolean addNode(V n);
 
     boolean deleteNode(V n);
 
     boolean searchNode(V n);
+
+    GNode<V> getNode(V i);
+
+    void setNode(V oldVal, V newVal);
+
+    void setNode(GNode<V> old, V newVal);
 
 //operazioni per archi
 
     boolean addEdge(GEdge<E,V> n);
 
     boolean removeEdge(GEdge<E,V> n);
+
+    GEdge<E,V> getEdge(V source, V destination);
+
+    void setEdge(V source, V destination, E newVal);
 
 }
--- src/prova/po/MyGraph.java
+++ src/prova/po/MyGraph.java
@@ -1,28 +1,4 @@
 package prova.po;
 
-public class MyGraph<V,E> implements Graph<V,E>{
-    @Override
-    public boolean aggiungiNode(V n) {
-        return false;
-    }
-
-    @Override
-    public boolean deleteNode(V n) {
-        return false;
-    }
-
-    @Override
-    public boolean searchNode(V n) {
-        return false;
-    }
-
-    @Override
-    public boolean addEdge(GEdge<E, V> n) {
-        return false;
-    }
-
-    @Override
-    public boolean removeEdge(GEdge<E, V> n) {
-        return false;
-    }
+public class MyGraph<V,E> {
 }

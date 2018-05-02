e7e6227877d9ae23a42211da8979a026cddf1a5c
moretz97 <moretto.riccardo9701@gmail.com>
2018-05-02 13:23:25 +0200

    Update class

--- src/prova/po/SpeedGraph.java
+++ src/prova/po/SpeedGraph.java
@@ -3,7 +3,7 @@ package prova.po;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class SpeedGraph<V,E> implements Graph<V,E>,Algorithm<V,E>{
+public class SpeedGraph<V,E> {
     private ArrayList<GNode<V>> nodi;
     private ArrayList<GEdge<E,V>> archi;
 
@@ -15,55 +15,5 @@ public class SpeedGraph<V,E> implements Graph<V,E>,Algorithm<V,E>{
     public SpeedGraph(ArrayList<GNode<V>> nodi, ArrayList<GEdge<E,V>> archi){
         this.nodi=nodi;
         this.archi=archi;
-    }
-
-    @Override
-    public boolean aciclico(Collection<E> v) {
-        return false;
-    }
-
-    @Override
-    public boolean addNode(V n) {
-        return false;
-    }
-
-    @Override
-    public boolean deleteNode(V n) {
-        return false;
-    }
-
-    @Override
-    public boolean searchNode(V n) {
-        return false;
-    }
-
-    @Override
-    public E getNode(V i) {
-        return null;
-    }
-
-    @Override
-    public void setNode(V oldVal, V newVal) {
-
-    }
-
-    @Override
-    public boolean addEdge(E n) {
-        return false;
-    }
-
-    @Override
-    public boolean removeEdge(E n) {
-        return false;
-    }
-
-    @Override
-    public E getEdge(V source, V destination) {
-        return null;
-    }
-
-    @Override
-    public void setEdge(V source, V destination, E newVal) {
-
     }
 }

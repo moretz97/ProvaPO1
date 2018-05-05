ac1939376775cd8ef84e01e57ee2696e8ed8868a
moretz97 <moretto.riccardo9701@gmail.com>
2018-05-05 12:25:14 +0200

    Da implementare i metodi dell'interfaccia

--- src/prova/po/SpeedGraph.java
+++ src/prova/po/SpeedGraph.java
@@ -3,7 +3,7 @@ package prova.po;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class SpeedGraph<V,E> {
+public class SpeedGraph<V,E> implements Algorithm<V,E>{
     private ArrayList<GNode<V>> nodi;
     private ArrayList<GEdge<E,V>> archi;
 
@@ -15,5 +15,105 @@ public class SpeedGraph<V,E> {
     public SpeedGraph(ArrayList<GNode<V>> nodi, ArrayList<GEdge<E,V>> archi){
         this.nodi=nodi;
         this.archi=archi;
+    }
+
+    @Override
+    public boolean aciclico(Collection<E> v) {
+        return false;
+    }
+
+    @Override
+    public boolean isconnected(Collection<V> v, Collection<E> a) {
+        return false;
+    }
+
+    @Override
+    public boolean isregular(Collection<V> v) {
+        return false;
+    }
+
+    @Override
+    public int[][] build_matrix(Collection<V> v) {
+        return new int[0][];
+    }
+
+    @Override
+    public boolean iscompleted(Collection<V> v, Collection<E> a) {
+        return false;
+    }
+
+    @Override
+    public Graph<V, E> findST(Collection<V> v, Collection<E> a) {
+        return null;
+    }
+
+    @Override
+    public Graph<V, E> findMST(Collection<V> v, Collection<E> a) {
+        return null;
+    }
+
+    @Override
+    public boolean addNode(V n) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteNode(V n) {
+        return false;
+    }
+
+    @Override
+    public boolean searchNode(V n) {
+        return false;
+    }
+
+    @Override
+    public E getNode(V i) {
+        return null;
+    }
+
+    @Override
+    public void setNode(V oldVal, V newVal) {
+
+    }
+
+    @Override
+    public int in_degree(V n) {
+        return 0;
+    }
+
+    @Override
+    public int out_degree(V n) {
+        return 0;
+    }
+
+    @Override
+    public int degree(V n) {
+        return 0;
+    }
+
+    @Override
+    public boolean isempty() {
+        return false;
+    }
+
+    @Override
+    public boolean addEdge(E n) {
+        return false;
+    }
+
+    @Override
+    public boolean removeEdge(E n) {
+        return false;
+    }
+
+    @Override
+    public E getEdge(V source, V destination) {
+        return null;
+    }
+
+    @Override
+    public void setEdge(V source, V destination, E newVal) {
+
     }
 }
